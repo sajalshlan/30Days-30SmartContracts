1e05e936bc62541f9c1ac393a6e5370bfccffb35
Sajal Sharma <sajalsharma2408@gmail.com>
2023-02-10 15:55:45 +0530

    done

---
+++ 03_PiggyBank.sol
@@ -0,0 +1,38 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+//to do :
+//  ser can depost in the wallet
+//  owner can withdraw the amount
+//  contract should be destroyed after withdrawal - self destroy function just after clickin on withdraw
+
+contract PiggyBank{
+    address payable immutable i_owner;
+
+    //event
+    event Deposit(uint amount);
+
+    constructor() {
+        i_owner = payable(msg.sender);
+    }
+
+    //function fund
+    function fund() payable public {
+        emit Deposit(msg.value);
+    }
+
+    //function withdraw and self-destroy
+    function withdraw() payable public {
+        // (bool success, ) = address(this).call{value: address(this).balance}("");
+        // require(success, "txn failed");
+
+        require(i_owner == msg.sender, "you are not the owner, FU");
+        selfdestruct(i_owner);
+        }
+
+        receive() external payable{
+            emit Deposit(msg.value);
+        }
+
+        fallback() external payable{}
+}
